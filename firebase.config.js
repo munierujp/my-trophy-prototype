f753f640e4afb5fa9ae5e57d8590dbfcf08b3d88
Munieru <20086673+munierujp@users.noreply.github.com>
2019-08-30 19:23:41 +0900

    Add firebase.config.js

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+// NOTE: It's safe to publish these values.
+export default {
+  apiKey: 'AIzaSyBo5ETZ220TnSk6J0aKi_6iFFuXSFAdYYM',
+  authDomain: 'my-trophy-prototype.firebaseapp.com',
+  databaseURL: 'https://my-trophy-prototype.firebaseio.com',
+  projectId: 'my-trophy-prototype',
+  storageBucket: '',
+  messagingSenderId: '514630523125',
+  appId: '1:514630523125:web:43937b95b3e2a3c2'
+}
