bb6f781f16191d2a83f981a8b0a9173164ac2c63
Munieru <20086673+munierujp@users.noreply.github.com>
2019-08-30 15:42:10 +0900

    Add stylelint (#3)
    * Add stylelint package
    
    `npm i -D stylelint`
    
    * Add @nuxtjs/stylelint-module package
    
    `npm i -D munierujp/stylelint-module#fix_spa_mode`
    
    * Add stylelint-scss package
    
    `npm i -D stylelint-scss`
    
    * Add stylelint-config-standard package
    
    `npm i -D stylelint-config-standard`
    
    * Add .stylelintrc.json
    
    * Add @nuxtjs/stylelint-module module
    
    * Add stylelint to lint-staged

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,6 +37,7 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
+    '@nuxtjs/stylelint-module',
     '@nuxtjs/vuetify'
   ],
   /*
