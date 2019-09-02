599506450b7708dc7b66d186653e2fcad51db38b
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-02 12:30:58 +0900

    Disable default font assets

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,8 @@ export default {
       { hid: 'description', name: 'description', content: lang.APP_DESCRIPTION }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto&display=swap' }
     ]
   },
   /*
