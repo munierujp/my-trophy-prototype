b5663b21dab998ef96b24c3a5c12a952a6190cd7
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-03 13:05:28 +0900

    Add AppDescription.vue

---
+++ components/AppDescription.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1 class="app-description">
+    {{ $t('APP_DESCRIPTION') }}
+  </h1>
+</template>
+
+<style lang="scss" scoped>
+$relative-max-width: 600px;
+$ratio: 0.06;
+$relative-font-size: 100vw * $ratio;
+$absolute-font-size: $relative-max-width * $ratio;
+
+.app-description {
+  overflow-wrap: break-word;
+  font-size: $absolute-font-size;
+}
+
+@media (max-width: $relative-max-width) {
+  .app-description {
+    font-size: $relative-font-size;
+  }
+}
+</style>
