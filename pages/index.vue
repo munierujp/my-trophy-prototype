f667877ec9521bef8d5f24b3c3dc1c5244e92afa
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-03 13:21:16 +0900

    Add index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,36 @@
 <template>
-  <v-layout />
+  <v-layout
+    column
+    align-center
+  >
+    <v-flex>
+      <app-title />
+    </v-flex>
+    <v-flex>
+      <app-description />
+    </v-flex>
+    <v-flex>
+      <app-button
+        :label="$t('START')"
+        color="secondary"
+        dark
+        to="/login"
+      />
+    </v-flex>
+  </v-layout>
 </template>
+
+<script>
+import AppButton from '~/components/AppButton'
+import AppDescription from '~/components/AppDescription'
+import AppTitle from '~/components/AppTitle'
+
+export default {
+  layout: 'noheader',
+  components: {
+    AppButton,
+    AppDescription,
+    AppTitle
+  }
+}
+</script>
