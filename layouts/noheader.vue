915baa55877a82dcc40de7cba3849109333753fa
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-03 12:41:13 +0900

    Add noheader layout (#13)

---
+++ layouts/noheader.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app id="app">
+    <v-content>
+      <v-container fluid>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Roboto, "Noto Sans JP", sans-serif;
+}
+</style>
