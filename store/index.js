2079168ab49ab4bc18373dfbe90128b7dfae5d7c
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-03 11:18:20 +0900

    Add auth (#9)
    * Add signIn.js
    
    * Add signInByGoogle.js
    
    * Add signInByGoogle action
    
    * Add signOut.js
    
    * Add signOut action
    
    * Add fetchAuth.js
    
    * Add fetchAuth action
    
    * Add fetchAuth action to nuxtClientInit action
    
    * Add requireAuthenticated.js
    
    * Add requireUnauthenticated.js

--- store/index.js
+++ store/index.js
@@ -1,10 +1,40 @@
 import Vuex from 'vuex'
+import signInByGoogle from '~/modules/signInByGoogle'
+import signOut from '~/modules/signOut'
+import fetchAuth from '~/modules/fetchAuth'
 
 export default function () {
   return new Vuex.Store({
+    state: () => ({
+      auth: null
+    }),
+    mutations: {
+      setAuth (state, auth) {
+        state.auth = auth
+      },
+      removeAuth (state) {
+        state.auth = null
+      }
+    },
     actions: {
-      nuxtClientInit ({ commit }, context) {
-        // Write initialization of client here
+      async nuxtClientInit ({ dispatch }, context) {
+        await dispatch('fetchAuth')
+      },
+      async fetchAuth ({ commit }) {
+        const auth = await fetchAuth()
+        if (auth) {
+          commit('setAuth', auth)
+        }
+      },
+      async signInByGoogle ({ commit }) {
+        const auth = await signInByGoogle()
+        if (auth) {
+          commit('setAuth', auth)
+        }
+      },
+      async signOut ({ commit }) {
+        await signOut()
+        commit('removeAuth')
       }
     }
   })
