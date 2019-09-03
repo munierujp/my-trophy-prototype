857b4880737d946354371f618f6d61a486b0b7d9
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-03 11:00:53 +0900

    Add signInByGoogle action

--- store/index.js
+++ store/index.js
@@ -1,10 +1,25 @@
 import Vuex from 'vuex'
+import signInByGoogle from '~/modules/signInByGoogle'
 
 export default function () {
   return new Vuex.Store({
+    state: () => ({
+      auth: null
+    }),
+    mutations: {
+      setAuth (state, auth) {
+        state.auth = auth
+      }
+    },
     actions: {
       nuxtClientInit ({ commit }, context) {
         // Write initialization of client here
+      },
+      async signInByGoogle ({ commit }) {
+        const auth = await signInByGoogle()
+        if (auth) {
+          commit('setAuth', auth)
+        }
       }
     }
   })
