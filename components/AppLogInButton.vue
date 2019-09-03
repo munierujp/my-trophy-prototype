8054f6a6f4fefac8abac8747ae0671d555e151e2
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-03 16:10:56 +0900

    Add AppLogInButton.vue

---
+++ components/AppLogInButton.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <app-button
+      v-bind="link"
+      @click="open"
+    />
+    <v-dialog
+      v-model="showDialog"
+      width="320px"
+    >
+      <v-card>
+        <v-card-title>
+          {{ $t('LOG_IN_DIALOG_TITLE') }}
+        </v-card-title>
+        <v-card-text>
+          {{ $t('LOG_IN_DIALOG_MESSAGE') }}
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <app-dialog-button
+            :label="$t('LOG_IN')"
+            @click="signIn"
+          />
+          <app-dialog-button
+            :label="$t('CLOSE')"
+            @click="close"
+          />
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import AppButton from '~/components/AppButton'
+import AppDialogButton from '~/components/AppDialogButton'
+
+export default {
+  components: {
+    AppButton,
+    AppDialogButton
+  },
+  data: () => ({
+    showDialog: false
+  }),
+  computed: {
+    auth () {
+      return this.$store.state.auth
+    },
+    link () {
+      return {
+        title: this.$t('START'),
+        label: this.$t('START'),
+        color: 'secondary',
+        dark: true
+      }
+    }
+  },
+  methods: {
+    open () {
+      if (this.auth) {
+        this.$router.push('/home')
+      } else {
+        this.showDialog = true
+      }
+    },
+    close () {
+      this.showDialog = false
+    },
+    signIn () {
+      this.$store.dispatch('signInByGoogle').then(() => {
+        this.$router.push('/home')
+      })
+    }
+  }
+}
+</script>
