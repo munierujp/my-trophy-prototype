86fbaccb907c92ca656f52b49bf02c5607c5773a
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-03 16:12:07 +0900

    Add AppLogInButton

--- pages/index.vue
+++ pages/index.vue
@@ -9,17 +9,22 @@
     <v-flex>
       <app-description />
     </v-flex>
+    <v-flex>
+      <app-log-in-button />
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
 import AppDescription from '~/components/AppDescription'
+import AppLogInButton from '~/components/AppLogInButton'
 import AppTitle from '~/components/AppTitle'
 
 export default {
   layout: 'noheader',
   components: {
     AppDescription,
+    AppLogInButton,
     AppTitle
   }
 }
