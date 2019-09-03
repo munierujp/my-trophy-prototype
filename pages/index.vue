47011e089ae507bc2853c99965162c8d642fc5c1
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-03 16:11:51 +0900

    Remove AppButton

--- pages/index.vue
+++ pages/index.vue
@@ -9,26 +9,16 @@
     <v-flex>
       <app-description />
     </v-flex>
-    <v-flex>
-      <app-button
-        :label="$t('START')"
-        color="secondary"
-        dark
-        to="/login"
-      />
-    </v-flex>
   </v-layout>
 </template>
 
 <script>
-import AppButton from '~/components/AppButton'
 import AppDescription from '~/components/AppDescription'
 import AppTitle from '~/components/AppTitle'
 
 export default {
   layout: 'noheader',
   components: {
-    AppButton,
     AppDescription,
     AppTitle
   }
