cec5140c9adc65e29266083591c34fefe9d95775
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-03 12:26:55 +0900

    Add header (#10)
    * Add LOG_OUT
    
    * Add AppHeader.vue
    
    * Add AppHeader

---
+++ components/AppHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app-bar
+    class="app-header"
+    app
+    color="primary"
+    dark
+  >
+    <v-toolbar-title>
+      {{ $t('APP_NAME') }}
+    </v-toolbar-title>
+    <template v-if="auth">
+      <v-spacer />
+      <v-menu
+        bottom
+        left
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn
+            icon
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item @click="signOut">
+            <v-list-item-title>{{ $t('LOG_OUT') }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </template>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  computed: {
+    auth () {
+      return this.$store.state.auth
+    }
+  },
+  methods: {
+    signOut () {
+      this.$store.dispatch('signOut').then(() => {
+        this.$router.push('/')
+      })
+    }
+  }
+}
+</script>
