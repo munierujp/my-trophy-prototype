7b8b716712c480ad002ccdd11589d16272dd5607
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-04 14:52:13 +0900

    Add user actions (#25)
    * Add user to state
    
    * Add fetchUser action
    
    * Add createUser action
    
    * Add fetchUserOrCreate action
    
    * Add fetchUserOrCreate action to signIn method
    
    * Add fetchUser action to nuxtClientInit action

--- store/index.js
+++ store/index.js
@@ -1,12 +1,16 @@
 import Vuex from 'vuex'
+import MyTrophyApi from '~/modules/MyTrophyApi'
 import signInByGoogle from '~/modules/signInByGoogle'
 import signOut from '~/modules/signOut'
 import fetchAuth from '~/modules/fetchAuth'
 
+const { API_ORIGIN } = process.env.config
+
 export default function () {
   return new Vuex.Store({
     state: () => ({
-      auth: null
+      auth: null,
+      user: null
     }),
     mutations: {
       setAuth (state, auth) {
@@ -14,11 +18,20 @@ export default function () {
       },
       removeAuth (state) {
         state.auth = null
+      },
+      setUser (state, user) {
+        state.user = user
+      },
+      removeUser (state) {
+        state.user = null
       }
     },
     actions: {
-      async nuxtClientInit ({ dispatch }, context) {
+      async nuxtClientInit ({ state, dispatch }, context) {
         await dispatch('fetchAuth')
+        if (state.auth) {
+          await dispatch('fetchUser')
+        }
       },
       async fetchAuth ({ commit }) {
         const auth = await fetchAuth()
@@ -35,6 +48,31 @@ export default function () {
       async signOut ({ commit }) {
         await signOut()
         commit('removeAuth')
+      },
+      async fetchUser ({ state, commit }) {
+        const { token, email } = state.auth
+        const api = MyTrophyApi.of(this.$axios, API_ORIGIN, token)
+        const user = await api.fetchUserByEmail(email)
+        if (user) {
+          commit('setUser', user)
+        }
+      },
+      async createUser ({ state, commit }) {
+        const { token, email } = state.auth
+        const api = MyTrophyApi.of(this.$axios, API_ORIGIN, token)
+        await api.createUser()
+        const user = await api.fetchUserByEmail(email)
+        if (user) {
+          commit('setUser', user)
+        } else {
+          throw new Error('Failed to create user')
+        }
+      },
+      async fetchUserOrCreate ({ state, dispatch }) {
+        await dispatch('fetchUser')
+        if (!state.user) {
+          await dispatch('createUser')
+        }
       }
     }
   })
