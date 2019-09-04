7c601a10b84d26ca0406e78451d8a3539a8deb94
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-04 15:50:17 +0900

    Add AppTrophyCard.vue

---
+++ components/AppTrophyCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card
+    :to="path"
+    width="320px"
+    max-width="100%"
+  >
+    <v-card-title primary-title>{{ title }}</v-card-title>
+    <v-card-text>{{ description }}</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    path () {
+      return `/trophy/${this.id}`
+    }
+  }
+}
+</script>
