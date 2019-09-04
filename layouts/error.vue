abf236e121d0880bebecd8b008ea7cd527a509ff
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-04 10:56:28 +0900

    Add error page (#21)
    * Add error.vue
    
    * Remove title
    
    * Add ERROR_MESSAGE_404
    
    * Add ERROR_MESSAGE_UNKNOWN
    
    * Update error message
    
    * Remove style
    
    * Remove link

---
+++ layouts/error.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app dark>
+    <h1>{{ message }}</h1>
+  </v-app>
+</template>
+
+<script>
+export default {
+  layout: 'empty',
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  computed: {
+    message () {
+      switch (this.error.statusCode) {
+        case 404:
+          return this.$t('ERROR_MESSAGE_404')
+        default:
+          return this.$t('ERROR_MESSAGE_UNKNOWN')
+      }
+    }
+  }
+}
+</script>
