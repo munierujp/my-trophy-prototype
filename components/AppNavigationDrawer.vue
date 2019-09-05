829c7476aceb658a6b12810785a32b4ac0b8bede
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-05 12:07:07 +0900

    Add AppNavigationDrawer.vue

---
+++ components/AppNavigationDrawer.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    app
+  >
+    <v-list>
+      <app-list-item
+        :title="$t('HOME')"
+        icon="mdi-home"
+        @click="toHome"
+      />
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+import AppListItem from '~/components/AppListItem'
+
+export default {
+  components: {
+    AppListItem
+  },
+  computed: {
+    drawer: {
+      get () {
+        return this.$store.state.drawer
+      },
+      set (drawer) {
+        this.$store.commit('setDrawer', drawer)
+      }
+    }
+  },
+  methods: {
+    toHome () {
+      this.$router.push(`/home`)
+    }
+  }
+}
+</script>
