244242d7be1758aac92b0f39641a312f98c193a5
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-05 23:48:19 +0900

    Add icons.js

---
+++ modules/icons.js
@@ -0,0 +1,9 @@
+// mdi: https://materialdesignicons.com/
+export default {
+  delete: 'mdi-delete',
+  edit: 'mdi-square-edit-outline',
+  home: 'mdi-home',
+  menu: 'mdi-dots-vertical',
+  twitter: 'mdi-twitter',
+  user: 'mdi-account-circle'
+}
