8733feb15a472c91edcb85f03037f3e8c7c4bf5c
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-05 12:00:31 +0900

    Add AppListItem.vue

---
+++ components/AppListItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-list-item @click="click">
+    <v-list-item-icon v-if="icon">
+      <v-icon>{{ icon }}</v-icon>
+    </v-list-item-icon>
+    <v-list-item-content>
+      <v-list-item-title>{{ title }}</v-list-item-title>
+      <v-list-item-subtitle v-if="subtitle">
+        {{ subtitle }}
+      </v-list-item-subtitle>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    subtitle: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    click (e) {
+      this.$emit('click', e)
+    }
+  }
+}
+</script>
