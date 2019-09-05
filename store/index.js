c7bc30d99f01ab7b06ff89ce7ef961c427f995f7
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-05 17:01:26 +0900

    Add removeUser to signOut action

--- store/index.js
+++ store/index.js
@@ -86,6 +86,7 @@ export default function () {
       },
       async signOut ({ state, commit }) {
         await signOut()
+        commit('removeUser')
         commit('removeAuth')
         state.api.clearToken()
       },
