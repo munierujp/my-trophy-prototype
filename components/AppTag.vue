13f72e0d018126389d3b232747f52a29be417721
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-05 14:56:23 +0900

    Add AppTag.vue

---
+++ components/AppTag.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-chip
+    :color="color"
+    :href="href"
+    :to="to"
+    :target="target"
+    link
+  >
+    <v-avatar left>
+      <v-icon>{{ icon }}</v-icon>
+    </v-avatar>
+    {{ label }}
+  </v-chip>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      default: ''
+    },
+    href: {
+      type: String,
+      default: ''
+    },
+    to: {
+      type: String,
+      default: ''
+    },
+    target: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
