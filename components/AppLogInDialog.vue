c71f851520c188d9b08b9466ed5f0e3aedbb2af9
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-07 12:06:06 +0900

    Add AppLogInDialog.vue

---
+++ components/AppLogInDialog.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-dialog
+    v-model="show"
+    width="320px"
+  >
+    <v-card>
+      <v-card-title>
+        {{ $t('LOG_IN_DIALOG_TITLE') }}
+      </v-card-title>
+      <v-card-text>
+        {{ $t('LOG_IN_DIALOG_MESSAGE') }}
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <app-dialog-button
+          :label="$t('LOG_IN')"
+          @click="signIn"
+        />
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import AppDialogButton from '~/components/AppDialogButton'
+
+export default {
+  components: {
+    AppDialogButton
+  },
+  props: {
+    value: {
+      type: Boolean,
+      required: true
+    }
+  },
+  computed: {
+    show: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  },
+  methods: {
+    close () {
+      this.show = false
+    },
+    async signIn () {
+      await this.$store.dispatch('signIn')
+      this.close()
+      this.$router.push('/home')
+    }
+  }
+}
+</script>
