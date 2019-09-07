f2977fa06529081fae86030dd57300176dec9ac8
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-08 01:12:04 +0900

    Add AppTextForm.vue

---
+++ components/elements/AppTextForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-text-field
+    v-model="computedValue"
+    :label="label"
+    :maxlength="maxLength"
+    :counter="maxLength"
+    :required="required"
+    :autofocus="autofocus"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    maxLength: {
+      type: Number,
+      required: true
+    },
+    required: {
+      type: Boolean,
+      default: false
+    },
+    autofocus: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    computedValue: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
