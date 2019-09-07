b2ade06953efed058356d9304c3b4d01041dff9b
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-07 20:09:02 +0900

    Add AppUserTag.vue

---
+++ components/items/AppUserTag.vue
@@ -0,0 +1,41 @@
+<template>
+  <app-tag
+    :label="name"
+    :icon="icons.user"
+    color="secondary"
+    :to="to"
+  />
+</template>
+
+<script>
+import icons from '~/modules/icons'
+import AppTag from '~/components/elements/AppTag'
+
+export default {
+  components: {
+    AppTag
+  },
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data: () => ({
+    icons
+  }),
+  computed: {
+    to () {
+      return this.link ? `/user/${this.id}` : ''
+    }
+  }
+}
+</script>
