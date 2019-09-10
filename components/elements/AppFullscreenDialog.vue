c4021679f258b6362cdfe2306504fafb98783cf8
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-10 23:32:47 +0900

    Add AppFullscreenDialog.vue

---
+++ components/elements/AppFullscreenDialog.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-dialog
+    v-model="show"
+    fullscreen
+    hide-overlay
+    transition="dialog-bottom-transition"
+  >
+    <slot />
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Boolean,
+      required: true
+    }
+  },
+  computed: {
+    show: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
