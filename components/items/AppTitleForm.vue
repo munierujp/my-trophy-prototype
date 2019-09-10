3174d1fc3997dad754ed843175fafb15b209a5f9
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-10 23:03:57 +0900

    Add AppTitleForm component (#62)
    * Add AppTitleForm.vue
    
    * Use AppTitleForm component

---
+++ components/items/AppTitleForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <app-text-form
+    v-model="title"
+    :label="$t('TITLE')"
+    :max-length="maxLength"
+    required
+    :autofocus="autofocus"
+  />
+</template>
+
+<script>
+import { trophy } from '~/modules/models'
+import AppTextForm from '~/components/elements/AppTextForm'
+
+export default {
+  components: {
+    AppTextForm
+  },
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    autofocus: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data: () => ({
+    maxLength: trophy.title.max
+  }),
+  computed: {
+    title: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
