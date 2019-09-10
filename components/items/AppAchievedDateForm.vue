7659aabbc1a4d77b0688bdaef2976946dcc37ad1
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-10 23:27:15 +0900

    Add AppAchievedDateForm component (#64)
    * Add AppAchievedDateForm.vue
    
    * Use AppAchievedDateForm component

---
+++ components/items/AppAchievedDateForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <app-date-form
+    v-model="achievedDate"
+    :label="$t('ACHIEVED_DATE')"
+    required
+    :autofocus="autofocus"
+  />
+</template>
+
+<script>
+import AppDateForm from '~/components/elements/AppDateForm'
+
+export default {
+  components: {
+    AppDateForm
+  },
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    autofocus: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    achievedDate: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
